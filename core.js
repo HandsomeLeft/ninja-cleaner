d68dc4981818ae839248d18e1c7808417834dc02
ceaser <2902465304@qq.com>
2024-01-02 11:31:59 +0800

    update async

--- core.js
+++ core.js
@@ -1,59 +1,58 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
-const { Logger } = require("./logger");
+const fs = require('fs').promises;
+const path = require('path');
+const { Logger } = require('./logger');
 
 const logger = new Logger();
 
 const strategies = {
-  directory: (filePath) => {
-    processDirectory(filePath);
+  directory: async (filePath) => {
+    await processDirectory(filePath);
   },
-  file: (filePath) => {
-    processFile(filePath);
+  file: async (filePath) => {
+    await processFile(filePath);
   },
   unknown: () => {
-    console.error("无效的文件路径");
+    console.error('无效的文件路径');
   },
 };
 
-
-function processDirectory(directoryPath) {
-  for (let file of fs.readdirSync(directoryPath)) {
+async function processDirectory(directoryPath) {
+  const files = await fs.readdir(directoryPath);
+  for (let file of files) {
     let filePath = path.join(directoryPath, file);
-    let stats = fs.statSync(filePath);
+    let stats = await fs.stat(filePath);
     if (stats.isDirectory()) {
-      processDirectory(filePath);
+      await processDirectory(filePath);
     } else if (stats.isFile()) {
-      processFile(filePath);
+      await processFile(filePath);
     }
   }
 }
 
-
-function processFile(filePath) {
-  let content = fs.readFileSync(filePath, "utf8");
+async function processFile(filePath) {
+  let content = await fs.readFile(filePath, 'utf8');
   let logStatements = [];
   content = content.replace(/console\.log\(.*?\);\n?/g, (match) => {
     logStatements.push(match);
-    return "";
+    return '';
   });
-  fs.writeFileSync(filePath, content);
+  await fs.writeFile(filePath, content);
   if (logStatements.length > 0) {
     logger.addLog(filePath, logStatements);
   }
 }
 
-function processPath(filePath) {
-  let stats = fs.statSync(filePath);
-  let type = "unknown";
+async function processPath(filePath) {
+  let stats = await fs.stat(filePath);
+  let type = 'unknown';
   if (stats.isDirectory()) {
-    type = "directory";
+    type = 'directory';
   } else if (stats.isFile()) {
-    type = "file";
+    type = 'file';
   }
   const strategy = strategies[type];
-  strategy(filePath);
+  await strategy(filePath);
   logger.printLogs();
 }
 

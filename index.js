b1b45d99ff03c4e1739ea59d21ef5dcc7cd2432e
ceaser <2902465304@qq.com>
2024-01-09 16:31:36 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
-const path = require("path");
 const { Command } = require('commander');
 const { processPath } = require("./core");
 
 const program = new Command();
-
 program
  .command("clean")
  .description("clean the specified file｜指定清理你的文件")
  .action(() => {
-   let filePath = process.cwd();
-   filePath = path.resolve(filePath);
+   const filePath = process.cwd(); // 直接使用 process.cwd() 获取当前工作目录
    processPath(filePath);
  });
 
